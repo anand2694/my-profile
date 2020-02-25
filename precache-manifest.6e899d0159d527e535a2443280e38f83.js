60eb9e30f91deee2aa1d9f70d7fcbb36d2305f95
Anand Singh Chauhanclear <anand@project42labs.com>
2020-02-25 16:11:48 +0530

    Updates

---
+++ precache-manifest.6e899d0159d527e535a2443280e38f83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c2eeed89474b5658e0169cddcfb03b",
+    "url": "/my-profile/index.html"
+  },
+  {
+    "revision": "2a114b336128fde78dd8",
+    "url": "/my-profile/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "42632dd9355ea27bbff9",
+    "url": "/my-profile/static/js/2.e6e85774.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-profile/static/js/2.e6e85774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a114b336128fde78dd8",
+    "url": "/my-profile/static/js/main.87b7aaf3.chunk.js"
+  },
+  {
+    "revision": "f91a0081b83213a97269",
+    "url": "/my-profile/static/js/runtime-main.47d2f2a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
